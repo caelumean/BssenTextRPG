cf69ed05383ad917e19cffbdc72c9edae076f97b
caelumean215 <98576510+caelumean@users.noreply.github.com>
2026-02-23 20:28:37 +0900

    [Update]
    player : character 기본 추상 클래스 생성

---
+++ BssenTextRPG/Models/Character.cs
@@ -0,0 +1,40 @@
+﻿namespace TextRPG.Models
+{
+    // 캐릭터 기본 추상 클래스
+    public abstract class Character
+    {
+        #region 프로퍼티
+        // protected : 해당 클래스 내에서 접근 가능 , 외부 클래스에선 접근 불가능
+        public string Name { get; protected set; }
+        public int CurrentHp {  get; protected set; }
+        public int CurrentMp { get; protected set; }
+        public int MaxHp { get; protected set; }
+        public int MaxMp {  get; protected set; }  
+        public int AttackPower {  get; protected set; }
+        public int Defense {  get; protected set; }
+        public int Level { get; protected set; }
+
+        // 생존 여부
+        public bool IsAlive => CurrentHp > 0;
+        #endregion
+
+        #region 생성자
+        protected Character(string name, int maxHp, int maxMp, int attackPower, int defense, int level)
+        {
+            Name = name;
+            MaxHp = maxHp;
+            CurrentHp = maxHp;
+            MaxMp = maxMp;
+            CurrentMp = maxMp;
+            AttackPower = attackPower;
+            Defense = defense;
+            Level = level;
+        }
+
+        #endregion
+
+        #region 메서드
+
+        #endregion
+    }
+}
